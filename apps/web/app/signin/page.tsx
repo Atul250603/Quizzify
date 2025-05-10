abd72970e6bf7c8523f8001eb126ec3493342a2c
Atul Goyal <atul250603@gmail.com>
2025-05-11 00:57:13 +0530

    Added the suspense fallback in the signin page

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -1,9 +1,17 @@
+import Loading from "@repo/ui/Loading";
 import SignInForm from "@repo/ui/SignInForm";
+import { Suspense } from "react";
 
 function SignIn() {
     return (
         <div>
-            <SignInForm/>
+            <Suspense fallback={
+                <div className="flex justify-center items-center h-screen">
+                    <Loading color="purple-600"/>
+                </div>
+            }>
+                 <SignInForm/>
+            </Suspense>
         </div>
     )
 }
